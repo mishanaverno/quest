ce76758c31299e56418158b76173b24023931e97
mishanaverni <mishanaverno@gmail.com>
2018-08-22 00:31:11 +0300

    удалил лишний символ

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_STRICT);
 define('DS', DIRECTORY_SEPARATOR); 
 define('ROOT_PATH', __DIR__.DS);
 define('CORE_PATH', ROOT_PATH.'core'.DS);
-require CORE_PATH.'core.php';;
+require CORE_PATH.'core.php';
